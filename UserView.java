9520c157f7b706fffdda5df32990743a107d38ab
Irina <andrey_k.90@mail.ru>
2023-10-31 00:34:55 +0300

    UserView commit

---
+++ UserView.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class UserView {
+    UserController controllerUser = new UserController();
+
+    public UserView() {
+        this.controllerUser = controllerUser;
+    }
+
+    public void saveGroup(TeacherModel teacherId, List<StudentModel> studentId, String name){
+        controllerUser.createSG(teacherId,studentId,name);
+    }
+
+    public void getGroup(String name){
+        System.out.println(controllerUser.getByName(name));
+    }
+
+}
